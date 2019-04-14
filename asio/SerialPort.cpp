532070903e4e7a8fb0edb1234db213225299f59e
walchko <walchko@users.noreply.github.com>
2019-04-14 08:06:08 -0400

    up

--- asio/SerialPort.cpp
+++ asio/SerialPort.cpp
@@ -9,7 +9,7 @@ using namespace boost;
 // https://www.boost.org/doc/libs/1_65_0/doc/html/boost_asio/reference/basic_serial_port.html
 // http://www.ridgesolutions.ie/index.php/2012/12/13/boost-c-read-from-serial-port-with-timeout-example/
 
-SerialPort::SerialPort(): io(), serial(io){}
+SerialPort::SerialPort(boost::asio::io_service& io): serial(io){}
 
 SerialPort::~SerialPort(){
     close();
@@ -32,6 +32,22 @@ bool SerialPort::open(const string& port, uint32_t baud_rate){
         cout << "*** SerialPort::open(): " << e.what() << endl;
         return false;
     }
+
+    // boost::asio::serial_port_base::flow_control flow_control;
+    // serial.get_option(flow_control);
+    //
+    // boost::asio::serial_port_base::baud_rate gbaud_rate;
+    // serial.get_option(gbaud_rate);
+    //
+    //
+    // serial_port_base::stop_bits stop_bits;
+    // serial.get_option(stop_bits);
+    //
+    // serial_port_base::character_size character_size;
+    // serial.get_option(character_size);
+    //
+    // cout << flow_control.value() << " " << character_size.value() << " " << gbaud_rate.value() << " " << stop_bits.value() << endl;
+    // exit(0);
     return true;
 }
 
@@ -39,55 +55,44 @@ int SerialPort::write(const string& s){
     return asio::write(serial, asio::buffer(s.c_str(),s.size()));
 }
 
-// int SerialPort::write(const std::string& s){}
-// size_t SerialPort::read(const size_t number){
-//     uint8_t b[number];
-//     size_t len = asio::read(serial, asio::buffer(&b, number));
-//     return len;
-// }
+int SerialPort::write(uint8_t *buff, const uint16_t num){
+    return asio::write(serial, asio::buffer(buff,num));
+}
 
 size_t SerialPort::read(const size_t number){
-    // uint8_t b[number];
     size_t len = asio::read(serial, asio::buffer(&buffer, number));
     return len;
 }
 
-// int SerialPort::read(string result){
-//     char c;
-//     // string result;
-//     // int num = 0;
-//     // for(;;){
-//     asio::read(serial, asio::buffer(&c,1));
-//     //     switch(c){
-//     //         case '\r':
-//     //             break;
-//     //         case '\n':
-//     //             return result.size();
-//     //         default:
-//     //             result += c;
-//     //     }
-//     // }
-//     return result.size();
-// }
+size_t SerialPort::read(const size_t number, uint8_t *buff){
+    size_t len = asio::read(serial, asio::buffer(buff, number));
+    return len;
+}
 
-// void SerialPort::dtr(bool val){
-//     // serial.EnableDTR(val);
-// }
+void SerialPort::setPin(bool enabled, int pin){
+    int fd = serial.native_handle();
+    if (enabled)
+        ioctl(fd, TIOCMBIS, &pin);
+    else
+        ioctl(fd, TIOCMBIC, &pin);
+}
 
 void SerialPort::setRTS(bool enabled){
-    int fd = serial.native_handle();
-    int data = TIOCM_RTS;
-    if (!enabled)
-        ioctl(fd, TIOCMBIC, &data);
-    else
-        ioctl(fd, TIOCMBIS, &data);
+    // int fd = serial.native_handle();
+    // int data = TIOCM_RTS;
+    // if (!enabled)
+    //     ioctl(fd, TIOCMBIC, &data);
+    // else
+    //     ioctl(fd, TIOCMBIS, &data);
+    setPin(enabled, TIOCM_RTS);
 }
 
 void SerialPort::setDTR(bool enabled){
-    int fd = serial.native_handle();
-    int data = TIOCM_DTR;
-    if (!enabled)
-        ioctl(fd, TIOCMBIC, &data);        // Clears the DTR pin
-    else
-        ioctl(fd, TIOCMBIS, &data);        // Sets the DTR pin
+    // int fd = serial.native_handle();
+    // int data = TIOCM_DTR;
+    // if (!enabled)
+    //     ioctl(fd, TIOCMBIC, &data);        // Clears the DTR pin
+    // else
+    //     ioctl(fd, TIOCMBIS, &data);        // Sets the DTR pin
+    setPin(enabled, TIOCM_DTR);
 }
--- asio/SerialPort.hpp
+++ asio/SerialPort.hpp
@@ -17,25 +17,26 @@
 class SerialPort
 {
 public:
-    SerialPort();
+    SerialPort(boost::asio::io_service& io);
     ~SerialPort();
 
     void close();
     bool open(const std::string& port, uint32_t baud_rate);
 
+    int write(uint8_t *s, const uint16_t num);
     int write(const std::string& s);
-    int write(const std::vector<uint8_t>& buffer){}
-    // void writeString(const std::string& s);
+    //int write(const std::vector<uint8_t>& buffer){}
 
-    std::array<std::uint8_t,1024> read(const size_t number=1024);
-    // int read(std::string);
+    size_t read(const size_t number);
+    size_t read(const size_t number, uint8_t *buffer);
 
     // void dtr(bool val);
+    // void setPin(bool enabled, int pin);
     void setRTS(bool enabled);
     void setDTR(bool enabled);
 
-// protected:
-    boost::asio::io_service io;
+protected:
+    void setPin(bool enabled, int pin);
     boost::asio::serial_port serial;
-    std::array<std::uint8_t,1024> buffer;
+    std::array<std::uint8_t, 1024> buffer;
 };
